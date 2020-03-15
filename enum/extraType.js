0d85612dabc3b94e538ad84c2732f8ef3347a2c9
Reactor94 <v.pylypets94@gmail.com>
2020-03-15 16:09:30 +0100

    Add extraType and SortType enums

---
+++ enum/extraType.js
@@ -0,0 +1,8 @@
+/**
+ * Extra types for filtering apartments
+ * @export
+ * @class FilterExtrasType
+ */
+export class FilterExtrasType {
+    static AIR_CONDITION = 'AIR_CONDITION';
+}
---
+++ enum/sortType.js
@@ -0,0 +1,19 @@
+/**
+ * Sorting types for apartment listing
+ * @export
+ * @class SortType
+ */
+export class SortType {
+
+    static DEFAULT = 'DEFAULT';
+
+    static QUALITY_SCORE = 'QUALITY_SCORE';
+
+    static NEWEST = 'NEWEST';
+
+    static LOWEST_PRICE = 'LOWEST_PRICE';
+
+    static HIGHER_PRICE = 'HIGHER_PRICE';
+
+    static AREA_HIGHEST = 'AREA_HIGHEST';
+}
